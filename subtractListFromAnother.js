dba2cf876c70cc98e05a219fad61d6c7b4622544
aaronmadsen1 <aaronmadsen2@gmail.com>
2020-01-08 18:15:41 -0700

    second commit

--- subtractListFromAnother.js
+++ subtractListFromAnother.js
@@ -17,11 +17,9 @@ array_diff([3, 3, 1, 2, 3, 2], [2, 4, 2, 1])
 function array_diff(a, b) {
   for (let i = 0; i < b.length; i++) {
     console.log(`i ${i} ${b[i]}`)
-
     for (let j = 0; j < a.length; j++) {
       console.log(`      j ${j} ${a[j]}`)
       console.log(`            List A ${a}`)
-
       if (a[j] === b[i]) {
         a.splice(j, 1)
         j--
